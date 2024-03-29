936afeb64325134e6aaf55fedc8294906e747608
lilia10010 <liliaxtz2@gmail.com>
2024-03-29 18:28:26 -0300

    add  and  files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,14 @@
-function status(request, response) {
-  /*  response.status(200).send("Vraaau LoL"); */
+import database from "../../../../infra/database.js";
 
-  response.json({ status: "Vraaau LoL" });
+async function status(request, response) {
+  console.log("🍄 >>>>>>> database:", database);
+  /*  response.status(200).send("Vraaau LoL"); */
+  /*  response.json({ status: "Vraaau LoL" }); */
+
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log("🍄 >>>>>>> status >>>>>> result::", result.rows);
+
+  response.status(200).json({ status: "Vraaau LoL" });
 }
 
 export default status;
