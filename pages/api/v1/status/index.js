48fc6041c5a690dfab619f1b67d569a35b19d65e
lilia10010 <liliaxtz2@gmail.com>
2024-04-08 17:10:55 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  console.log("🍄 >>>>>>> database", database);
-  /*  response.status(200).send("Vraaau LoL"); */
-  /*  response.json({ status: "Vraaau LoL" }); */
+  const updateAt = new Date().toISOString();
 
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log("🍄 >>>>>>> status >>>>>> result:", result.rows);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  response.status(200).json({ status: "Vraaau LoL" });
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
